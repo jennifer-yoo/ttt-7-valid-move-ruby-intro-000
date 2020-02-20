346109e754f1425dc2b4b4984d406a2008130d15
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-02-20 18:02:01 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board,index)
   if position_taken?(board,index)
     true
-  else position_taken?(board, index.between?(0, 8))
+  else position_taken?(board, index.between?(0, 9))
     false
   end
 end
